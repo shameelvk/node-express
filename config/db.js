ee0f1328e344aee418180d175e5f682ca9d7dc76
MOHAMMED SHAMEEL <84220211+shameelvk@users.noreply.github.com>
2024-11-03 23:49:28 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,3 @@
 
-const { Pool } =require("pg");
 
-const pool=new Pool({
-    host: 'localhost',
-    database: 'training_db',
-   password:"shameel786",
-   port:5432,
-   user:"training_user"
-  })
-
-module.exports=pool;
  
